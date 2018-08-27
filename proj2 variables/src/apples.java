6c0c128a53aa2916c3b7fee400d2579dc65f5eb1
ychang79 <ychang360@gmail.com>
2018-08-26 21:24:44 -0400

    cm2

---
+++ proj2 variables/src/apples.java
@@ -0,0 +1,12 @@
+
+public class apples {
+	public static void main(String args []) {
+		double tuna;
+		tuna = 5.28;
+		
+		System.out.print("i want ");
+		System.out.print(tuna);
+		System.out.println(" movies"); //println moves cursor to next line
+		System.out.print("apples");
+	}
+}
