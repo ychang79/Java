9e499346722b923eb103e8ac11a432015139dbce
ychang79 <ychang360@gmail.com>
2018-08-27 22:27:00 -0400

    cm5

---
+++ proj5 if statement/src/apples.java
@@ -0,0 +1,13 @@
+
+public class apples {
+	public static void main(String args[]) {
+		int test = 9;
+		
+		if (2) {
+			System.out.println("yess");
+		}else {
+			System.out.println("noooo");
+		}
+		
+	}
+}
