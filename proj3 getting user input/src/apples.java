f4f6c66f148c2edd67f408057cfe84c787d1cfea
ychang79 <ychang360@gmail.com>
2018-08-27 22:25:17 -0400

    cm3

---
+++ proj3 getting user input/src/apples.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class apples {
+	public static void main(String args[]){
+		Scanner buckky = new Scanner(System.in); //keybord input in scanner variable buckky
+		System.out.println(buckky.nextLine());
+	
+	}
+}
