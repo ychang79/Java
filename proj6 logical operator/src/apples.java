147a6ef19928453fb205eff5ecd6b85e7683f031
ychang79 <ychang360@gmail.com>
2018-09-07 21:44:34 -0400

    cm2

---
+++ proj6 logical operator/src/apples.java
@@ -0,0 +1,16 @@
+
+public class apples {
+	  public static void main(String[] args)
+	  {
+	   int boy, girl;
+	   boy = 18;
+	   girl = 68;
+	   
+	   if(boy > 10 || girl < 60){			//&& and || or
+	   System.out.println("you can enter");
+	   }else{
+	   System.out.println("you cannot enter");
+	   }
+	   
+	  }
+}
